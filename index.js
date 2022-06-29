1bf21097ac7e262ab21df30509900c3cce533b61
UnrHim48 <107967169+UnrHim48@users.noreply.github.com>
2022-06-29 21:22:43 +0200

    initial-commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var randomNumber1, randomNumber2;
+randomNumber1=Math.floor(Math.random() * 6) + 1;
+randomNumber2=Math.floor(Math.random() * 6) + 1;
+
+//num1
+if(randomNumber1 == 1){
+  document.querySelector('img.img1').setAttribute("src","images/dice1.png");
+}
+if(randomNumber1 == 2){
+document.querySelector('img.img1').setAttribute("src","images/dice2.png");
+}
+if(randomNumber1 == 3){
+  document.querySelector('img.img1').setAttribute("src","images/dice3.png");
+}
+if(randomNumber1 == 4){
+  document.querySelector('img.img1').setAttribute("src","images/dice4.png");
+}
+if(randomNumber1 == 5){
+document.querySelector('img.img1').setAttribute("src","images/dice5.png");
+}
+
+if(randomNumber1 == 6){
+  document.querySelector('img.img1').setAttribute("src","images/dice6.png");
+}
+
+//num2
+if(randomNumber2 == 1){
+  document.querySelector('img.img2').setAttribute("src","images/dice1.png");
+}
+if(randomNumber2 == 2){
+document.querySelector('img.img2').setAttribute("src","images/dice2.png");
+}
+if(randomNumber2 == 3){
+  document.querySelector('img.img2').setAttribute("src","images/dice3.png");
+}
+if(randomNumber2 == 4){
+  document.querySelector('img.img2').setAttribute("src","images/dice4.png");
+}
+if(randomNumber2 == 5){
+document.querySelector('img.img2').setAttribute("src","images/dice5.png");
+}
+if(randomNumber2 == 6){
+  document.querySelector('img.img2').setAttribute("src","images/dice6.png");
+}
+
+//display text
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="Player 1 Wins";
+}
+ if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent="Player 2 Wins";
+}
+if(randomNumber1==randomNumber2){
+  document.querySelector("h1").textContent="Draw";
+}
+//function
+function refresh(){
+    window.location.reload();
+}
